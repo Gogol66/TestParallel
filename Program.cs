ce8115e023d721852ecf4f94f6826cc8de6f4cf0
Gogol <Gogol@DESKTOP-DPFIS64>
2025-06-15 11:23:36 +0530

    Added Threadpool.QueUserWorkItem functionality

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TestParallel
@@ -13,6 +14,8 @@ namespace TestParallel
             Task t1 = Task.Run(() => Show());
             Task t2 = Task.Run(() => Print());
             Task.WaitAll(t1, t2);
+            for (int i = 0; i < 20; i++)
+                ThreadPool.QueueUserWorkItem(WorkItem, i);
             Console.ReadKey();
         }
         static async void Show()
@@ -24,6 +27,10 @@ namespace TestParallel
         {
             await Task.Delay(100);
             Console.WriteLine($"Print function");
+        }
+        static void WorkItem(object state)
+        {
+            Console.WriteLine($"Task {state} running on thread {Thread.CurrentThread.ManagedThreadId}");
         }
     }
 }
